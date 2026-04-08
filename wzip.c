c26cf2057bc7d2357fcf443e1c3331a91684d54f
Tom Gehman <tomgehman1@gmail.com>
2026-04-07 20:15:07 -0400

    test2 passed

--- wzip.c
+++ wzip.c
@@ -9,14 +9,15 @@ int main(int argc, char**argv) {
     }
 
     FILE *fp;
+    int last=-1, ch, ct=0;
 
     for (int i=1; i<argc; i++){
         if (! (fp = fopen(argv[i], "r"))) {
             fprintf(stderr, "failed to open file\n");
             exit(1);
         }
-        int last, ch, ct=0;
-        last=ch=fgetc(fp);
+        ch=fgetc(fp);
+        if (last==-1) last=ch;
         while (ch != EOF){
             if (ch!=last){
                 fwrite(&ct, sizeof(int), 1, stdout);
@@ -28,9 +29,9 @@ int main(int argc, char**argv) {
             }
             ch = fgetc(fp);
         }
-        fwrite(&ct, sizeof(int), 1, stdout);
-        fputc(last, stdout);
     }
+    fwrite(&ct, sizeof(int), 1, stdout);
+    fputc(last, stdout);
             
 
     return 0;
