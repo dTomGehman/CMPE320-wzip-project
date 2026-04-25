797d464c09177e3ab78694f2b88c37392509507a
Tom Gehman <tomgehman1@gmail.com>
2026-04-25 12:33:55 -0400

    encapsulated single-threaded file counting into function

--- makefile
+++ makefile
@@ -2,5 +2,5 @@ wzip: wzip.o
 	gcc -o wzip wzip.o -Wall -Werror
 
 wzip.o: wzip.c
-	gcc -c wzip.c -Wall -Werror
+	gcc -c wzip.c -Wall -Werror -pthread
 
--- wzip.c
+++ wzip.c
@@ -2,21 +2,40 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+
+typedef struct {
+    int ch;
+    int ct;
+} rettype;
+
+rettype proc_file(FILE *fp, int last, int ct);
 int main(int argc, char**argv) {
     if (argc < 2) {
         printf("wzip: file1 [file2 ...]\n");
         exit(1);
     }
-
+    rettype out;
+    out.ch=-1;
+    out.ct=0;
     FILE *fp;
-    int last = -1, ch, ct = 0;
+
+//    int last = -1, ch, ct = 0;
 
     for (int i = 1; i < argc; i++) {
         if (! (fp = fopen(argv[i], "r"))) {
             fprintf(stderr, "failed to open file\n");
             exit(1);
         }
-        ch = fgetc(fp);
+        out = proc_file(fp, out.ch, out.ct);
+    }
+    fwrite(&(out.ct), sizeof(int), 1, stdout);
+    fputc(out.ch, stdout);
+            
+    return 0;
+
+}
+rettype proc_file(FILE *fp, int last, int ct){
+        int ch = fgetc(fp);
         if (last == -1) last = ch;
         while (ch != EOF) {
             if (ch != last) {
@@ -29,12 +48,9 @@ int main(int argc, char**argv) {
             }
             ch = fgetc(fp);
         }
-    }
-    fwrite(&ct, sizeof(int), 1, stdout);
-    fputc(last, stdout);
-            
-
-    return 0;
-
+        rettype out;
+        out.ch=last;
+        out.ct=ct;
+        return out;
 }
-
+ 
